4e0b6f55199a77a04f92fa39b1814b75d777ea2b
Yichen Mo <amomorning@gmail.com>
2020-09-11 13:52:15 +0800

    Add calendar

--- dxf_reader.py
+++ dxf_reader.py
@@ -1,6 +1,6 @@
 import ezdxf
 import matplotlib.pyplot as plt
-
+import calendar
 
 doc = ezdxf.readfile("template.dxf")
 msp = doc.modelspace()
@@ -10,7 +10,7 @@ inner_down = msp.query('POLYLINE[layer=="inner_down"]')
 dot = msp.query('POLYLINE[layer=="dot"]')  
 outer = msp.query('POLYLINE[layer=="outer"]')
 
-f = plt.figure(figsize=(12, 9))
+f = plt.figure(figsize=(24, 18))
 for o in outer:
     for i in range(len(o)-1):
         u = o[i].dxf.location[0:2] 
@@ -23,6 +23,46 @@ for o in dot:
     print(u, v)
     plt.plot([u[0], v[0]], [u[1], v[1]], 'k--')
 
+cnt = 1
+print('up', len(inner_up))
+for o in inner_up:
+    x = 0
+    y = 0
+    for i in range(len(o)):
+        x += o[i].dxf.location[0]
+        y += o[i].dxf.location[1]
+    x /= len(o)
+    y /= len(o)
+    # plt.scatter(x, y, color='r')
+    t = str(calendar.month(2020, cnt))
+    cnt += 1
+    plt.text(x, y+3.6, t, family='consolas', fontsize=14, ha='center', ma='left', va='top',linespacing=1.6, wrap=False)
+
+    # for i in range(len(o)):
+    #     u = o[i].dxf.location[0:2] 
+    #     v = o[(i+1)%len(o)].dxf.location[0:2]
+    #     plt.plot([u[0], v[0]], [u[1], v[1]], color='k')
+
+
+for o in inner_down:
+    x = 0
+    y = 0
+    for i in range(len(o)):
+        x += o[i].dxf.location[0]
+        y += o[i].dxf.location[1]
+    x /= len(o)
+    y /= len(o)
+    # plt.scatter(x, y, color='r')
+    t = str(calendar.month(2020, cnt))
+    cnt += 1
+    plt.text(x, y+4.8, t, family='consolas', fontsize=14, rotation=180, ha='center', ma='left', va='top',linespacing=1.6, wrap=False)
+
+    # for i in range(len(o)):
+    #     u = o[i].dxf.location[0:2] 
+    #     v = o[(i+1)%len(o)].dxf.location[0:2]
+    #     plt.plot([u[0], v[0]], [u[1], v[1]], color='k')
+
+
 plt.axis('off')
 plt.show()
-f.savefig("foo.pdf", bbox_inches='tight')
+# f.savefig("foo.pdf", bbox_inches='tight')
--- gen_calendar.py
+++ gen_calendar.py
@@ -1,2 +1,6 @@
 import calendar
-print(calendar.month(2020, 6))
+print(calendar.month(2020, 9))
+s = str(calendar.month(2020, 9))
+
+for i in range(len(s)):
+    print(s[i])
