61b0b2e16742a1fe51476c948020366dcd74729d
Yichen Mo <amomorning@gmail.com>
2020-09-14 13:38:37 +0800

    Add date picker

---
+++ calendar-web/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+    >
+      <div class="d-flex">
+        <v-img
+          alt="Dodecahedron"
+          class="shrink mr-2"
+          contain
+          src="@/assets/dodecahedron.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <h1 class="text-darken-1 ma-2"> dodecahedron calendar </h1>
+
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/amomorning/dodecahedron-calendar"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Github</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+
+    <v-main>
+      <DatePicker/>
+    </v-main>
+
+    <v-footer
+      absolute
+      class="font-weight-medium"
+    >
+      <v-col
+        class="text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Amomorning</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import DatePicker from './components/DatePicker';
+
+export default {
+  name: 'App',
+
+  components: {
+    DatePicker,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ calendar-web/src/components/DatePicker.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-row
+    absolute
+  >
+    <v-col cols="6" sm="3" offset="1">
+      <v-date-picker
+        v-model="dates"
+        multiple
+        landscape
+        color="black"
+      ></v-date-picker>
+    </v-col>
+    <v-col cols="6" sm="3">
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="dates"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+        color="black"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-combobox
+            v-model="dates"
+            multiple
+            chips
+            small-chips
+            label="Multiple picker in menu"
+            prepend-icon="event"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+            color="black"
+          ></v-combobox>
+        </template>
+        <v-date-picker v-model="dates" multiple no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="black" @click="menu = false">Cancel</v-btn>
+          <v-btn text color="black" @click="$refs.menu.save(dates)">OK</v-btn>
+        </v-date-picker>
+      </v-menu>
+    </v-col>
+  </v-row>
+</template>
+<script>
+  export default {
+    data: () => ({
+      dates: ['2018-09-15', '2018-09-20'],
+      menu: false,
+    }),
+  }
+</script>
