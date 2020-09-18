155136e6154bb0426a98b2bab67afc7881ed3386
Yichen Mo <amomorning@gmail.com>
2020-09-18 13:58:53 +0800

    Format code

--- calendar-web/src/App.vue
+++ calendar-web/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="white"
-    >
+    <v-app-bar app color="white">
       <div class="d-flex">
         <v-img
           alt="Dodecahedron"
@@ -13,111 +10,94 @@
           transition="scale-transition"
           width="40"
         />
-        <h1 class="text-darken-1 ma-2"> dodecahedron calendar </h1>
-
+        <h1 class="text-darken-1 ma-2">dodecahedron calendar</h1>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/amomorning/dodecahedron-calendar"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/amomorning/dodecahedron-calendar" target="_blank" text>
         <span class="mr-2">Github</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
-
     <v-main>
-<v-card 
-  width="360" 
-  align="center" 
-  justify="center" 
-  elevation="0"
-  class="ma-10">
-      <DatePicker ref="dates"/>
+      <v-card width="360" align="center" justify="center" elevation="0" class="ma-10">
+        <DatePicker ref="dates" />
 
-      <vue-select-image
-  :dataImages="dataImages"
-  h='90px'
-  w='90px'
-  @onselectimage="onSelectImage"
-  ref='mainstyle'
-  >
-</vue-select-image>
-      <ColorPicker initialColor="#E1ECF4" ref="maincolor"> </ColorPicker>
-      <ColorPicker initialColor="#E45C18" ref="percolor"> </ColorPicker>
+        <vue-select-image
+          :dataImages="dataImages"
+          h="90px"
+          w="90px"
+          @onselectimage="onSelectImage"
+          ref="mainstyle"
+        ></vue-select-image>
+        <ColorPicker initialColor="#E1ECF4" ref="maincolor"></ColorPicker>
+        <ColorPicker initialColor="#E45C18" ref="percolor"></ColorPicker>
 
-      <v-row justify="center" class="ma-4">
-          <v-btn dark @click="collectData"> Update</v-btn>
-      </v-row>
+        <v-row justify="center" class="ma-4">
+          <v-btn dark @click="collectData">Update</v-btn>
+        </v-row>
       </v-card>
     </v-main>
 
-    <v-footer
-      absolute
-      class="font-weight-medium"
-    >
-      <v-col
-        class="text"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Amomorning</strong>
+    <v-footer absolute class="font-weight-medium">
+      <v-col class="text" cols="12">
+        {{ new Date().getFullYear() }} —
+        <strong>Amomorning</strong>
       </v-col>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import DatePicker from './components/DatePicker';
-import ColorPicker from './components/ColorPicker'
-import VueSelectImage from 'vue-select-image'
+import DatePicker from "./components/DatePicker";
+import ColorPicker from "./components/ColorPicker";
+import VueSelectImage from "vue-select-image";
 // add stylesheet
-require('vue-select-image/dist/vue-select-image.css')
+require("vue-select-image/dist/vue-select-image.css");
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     DatePicker,
     ColorPicker,
-    VueSelectImage 
+    VueSelectImage,
   },
 
   data: () => ({
-    dataImages:[{
-  id: '1',
-  src: '/img/filled.png',
-  alt: 'filled'
-}, {
-  id: '2',
-  src: '/img/doubled.png',
-  alt: 'doubled'
-}, {
-  id: '3',
-  src: '/img/flipped.png',
-  alt: 'flipped'
-}]
-
+    dataImages: [
+      {
+        id: "1",
+        src: "/img/filled.png",
+        alt: "filled",
+      },
+      {
+        id: "2",
+        src: "/img/doubled.png",
+        alt: "doubled",
+      },
+      {
+        id: "3",
+        src: "/img/flipped.png",
+        alt: "flipped",
+      },
+    ],
   }),
   methods: {
     onSelectImage: function (data) {
-      this.imageSelected = data
+      this.imageSelected = data;
     },
-    collectData : function() {
-      console.log(this.$refs.dates.dates.slice(0,));
+    collectData: function () {
+      console.log(this.$refs.dates.dates.slice(0));
       console.log(this.$refs.maincolor.color);
       console.log(this.$refs.percolor.color);
-      var select =this.imageSelected;
-      if(typeof(select) != 'undefined')
-        console.log(select.alt);
-
-    }
+      var select = this.imageSelected;
+      if (typeof select != "undefined") console.log(select.alt);
+    },
   },
   mounted() {
     console.log(DatePicker.data().dates);
-    
-  }
+  },
 };
 </script>
--- calendar-web/src/components/DatePicker.vue
+++ calendar-web/src/components/DatePicker.vue
@@ -1,57 +1,48 @@
 <template>
-<v-row>
-    <v-col 
-      class="pl-5">
-      <v-date-picker
-        v-model="dates"
-        multiple
-        color="black"
-      ></v-date-picker>
+  <v-row>
+    <v-col class="pl-5">
+      <v-date-picker v-model="dates" multiple color="black"></v-date-picker>
     </v-col>
-  <v-col 
-        class="px-10"
-  >
-    <v-row >
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="dates"
-        transition="scale-transition"
-        offset-y
-        min-width="280px"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-combobox
-            v-model="dates"
-            multiple
-            chips
-            small-chips
-            label="Multiple picker in menu"
-            prepend-icon="mdi-calendar-blank"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-            color="black"
-          ></v-combobox>
-        </template>
-        <v-date-picker v-model="dates" multiple no-title scrollable>
-          <v-spacer></v-spacer>
-          <v-btn text color="black" @click="menu = false">Cancel</v-btn>
-          <v-btn text color="black" @click="$refs.menu.save(dates)">OK</v-btn>
-        </v-date-picker>
-      </v-menu>
+    <v-col class="px-10">
+      <v-row>
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="dates"
+          transition="scale-transition"
+          offset-y
+          min-width="280px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-combobox
+              v-model="dates"
+              multiple
+              chips
+              small-chips
+              label="Multiple picker in menu"
+              prepend-icon="mdi-calendar-blank"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+              color="black"
+            ></v-combobox>
+          </template>
+          <v-date-picker v-model="dates" multiple no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="black" @click="menu = false">Cancel</v-btn>
+            <v-btn text color="black" @click="$refs.menu.save(dates)">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
       </v-row>
-
     </v-col>
-</v-row> 
+  </v-row>
 </template>
 <script>
-  export default {
-    data: () => ({
-      dates: [new Date().getFullYear() +'-09-01'],
-      menu: false,
-    }),
-
-  }
+export default {
+  data: () => ({
+    dates: [new Date().getFullYear() + "-09-01"],
+    menu: false,
+  }),
+};
 </script>
